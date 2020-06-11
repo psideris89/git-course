bfa0fb807b7e4dba4045e2e15e8b7eedfbfe9edf
psideris <psideris1989@gmail.com>
2020-06-11 14:43:14 +0100

    Exercise 2

---
+++ exercise2/README.md
@@ -0,0 +1,8 @@
+## Git Course
+
+### Exercise 2 - Git Reset
+
+- create 3 new commits
+- reset to initial commit and keep changes
+- create 1 new commit
+- reset to initial commit without persisting the changes
---
+++ exercise2/app.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function addNumber(a) {
+    return a + 5;
+}
+
+function subNumber(a) {
+    return a - 5;
+}
+
+result1 = add(10, 5);
+console.log("Add result:", result1);
+
+result2 = sub(28, 3);
+console.log("Sub result:", result2);
+
+result3 = addNumber(20);
+console.log("AddNumber result:", result3);
+
+result4 = subNumber(20);
+console.log("SubNumber result:", result4);
+
